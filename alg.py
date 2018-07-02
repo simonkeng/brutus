e8057ccd5479674a040f6161ab947cadff7753da
Simon Keng <simonkeng7@gmail.com>
2018-07-01 17:14:04 -0700

    added comments

--- alg.py
+++ alg.py
@@ -2,16 +2,18 @@ import string
 import random
 from termcolor import colored
 
+# 26 letters
 options = string.ascii_uppercase
 
 store = dict()
 itr = 0
 
-# 500,000 iterations to find them all
+# 500,000 iterations
 iterations = 500000
 
 for i in range(iterations):
 
+    # k=3 for 3 letter codes
     guess_list = random.choices(options, k=3)
     guess = ''.join(guess_list)
     itr += 1
@@ -21,12 +23,12 @@ for i in range(iterations):
         store[itr] = guess
         print('New code found & added to store: ', colored(guess, 'green'))
 
-
     else:
         print('Already in store: ', colored(guess, 'red'))
 
-final = set(store.values())
 
+
+final = set(store.values())
 print('Final store size: ', len(final))
 
 with open('store.txt', 'w') as f:
