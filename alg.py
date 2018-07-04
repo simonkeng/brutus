3174e8858faff780e6676e864c8685e7d5df8183
Simon Keng <simonkeng7@gmail.com>
2018-07-03 23:53:27 -0700

    added files from a different application of this algorithm, finding the words from a wordscape crossword puzzle

--- alg.py
+++ alg.py
@@ -2,19 +2,18 @@ import string
 import random
 from termcolor import colored
 
-# 26 letters
-options = string.ascii_uppercase
+# 823,543
+
+options = "SUNIOUS"
 
 store = dict()
 itr = 0
 
-# 100,000 iterations
-iterations = 300000
+iterations = 900000
 
 for i in range(iterations):
 
-    # k=3 for 3 letter codes
-    guess_list = random.choices(options, k=3)
+    guess_list = random.choices(options, k=7)
     guess = ''.join(guess_list)
     itr += 1
 
@@ -28,7 +27,11 @@ for i in range(iterations):
         print('Code already found: ', colored(guess, 'red'))
         print('On iteration: ', itr)
 
-
+    
+    if iterations == 200000:
+        with open('200k-check.txt', 'w') as f:
+            fin = set(store.values())
+            f.write(str(fin))
 
 final = set(store.values())
 print('Final number of unique codes found: ', len(final))
@@ -38,3 +41,4 @@ with open('data.txt', 'w') as f:
     f.write(str(final))
     f.write('\nNumber of codes found:\n')
     f.write(str(len(final)))
+
